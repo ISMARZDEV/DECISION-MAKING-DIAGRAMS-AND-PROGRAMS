d422fdd4e2937aaffc9072e9f3a63a46e0651c35
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 22:51:41 -0400

    Create logicalexercise1.cs

---
+++ DECISION-MAKING-PROGRAMS/logicalexercise1.cs
@@ -0,0 +1,30 @@
+//Pida dos números y determine el mayor de ellos;
+
+using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Digite su 1er numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite su 2do numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"El numero {num1} es mayor que {num2}");
+
+            }
+
+            else
+            {
+                Console.WriteLine($"El numero {num2} es mayor que {num1}");
+            }
+        }
+    }
+}
